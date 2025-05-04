99e019177e2d5e53b5553c8f94fdfb1be99f72f3
Yead191 <yead191@gmail.com>
2025-05-04 16:22:13 +0600

    added sonner

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Router'
-import { Toaster } from 'react-hot-toast'
-import { HelmetProvider } from 'react-helmet-async'
-import AuthProvider from './Provider/AuthProvider'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Router";
+import { Toaster as HotToaster } from "react-hot-toast";
+import { Toaster as ShadToaster } from "sonner";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Provider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-
         <HelmetProvider>
-          <Toaster></Toaster>
+          <HotToaster />
+          <ShadToaster richColors position="top-right" />
           <RouterProvider router={router} />
         </HelmetProvider>
       </QueryClientProvider>
     </AuthProvider>
-
-
-  </StrictMode>,
-)
+  </StrictMode>
+);
